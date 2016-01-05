055dd463d4b18805b7c3f8a2e48552d848f8a837
Andreas Larsen <andreas@larsenwork.com>
2016-01-05 20:05:55 +0100

    encoding

--- published_between.py
+++ published_between.py
@@ -57,10 +57,8 @@ def get_object_metadata(object, metadata_guid):
 
 
 def or_empty(v):
-    if type(v) == str:
+    if type(v) == str or type(v) == unicode:
         return v.encode('utf8')
-    elif type(v) == unicode:
-        return v
     elif type(v) == int:
         return v
     else:
